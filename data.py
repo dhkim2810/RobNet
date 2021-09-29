0336d4df00a3999c78ff4b22136c2fff03a5f4be
Chuck <dhkim2810@gmail.com>
2021-09-29 08:18:11 +0000

    add dataset class

---
+++ data.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms, datasets
+
+def load_data(args, apply_da=True):
+    """Load CIFAR10 Dataset"""
+    batch_size = args.batch_size
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    train_transforms = transforms.Compose([
+        # transforms.RandomAffine(degrees=(30, 70), translate=(0.1, 0.3), scale=(0.5, 0.75)),
+        transforms.RandomCrop(size=(32,32), padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        normalize])
+    test_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        normalize])
+    
+    if not apply_da:
+        train_transforms = test_transforms
+        batch_size = 1
+    
+    train_dataset = datasets.CIFAR10(root=args.data_dir, train=True, transform=train_transforms, download=True)
+    test_dataset = datasets.CIFAR10(root=args.data_dir, train=False, transform=test_transforms, download=True)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=args.num_workers, pin_memory=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=args.num_workers, pin_memory=True)
+
+    return train_loader, test_loader
+
+
+class PoisonedDataset(Dataset):
+    def __init__(self, args):
+        super(PoisonedDataset, self).__init__()
+        self.args = args
+        args.attack_type = self.attack_type # Single Trigger /  Multi Trigger
+        args.mask_type = self.mask_type     # Single Mask    /  Multi Mask
+        
+    def init_mask(self
